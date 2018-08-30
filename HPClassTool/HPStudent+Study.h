ae3506aa7c244653da7a591d96378e8708f903cc
王辉平 <WANGHUIPING032@pingan.com.cn>
2018-08-30 17:16:23 +0800

    增加分类属性

---
+++ HPClassTool/HPStudent+Study.h
@@ -0,0 +1,13 @@
+//
+//  HPStudent+Study.h
+//  HPClassTool
+//
+//  Created by 王辉平 on 2018/8/30.
+//  Copyright © 2018年 王辉平. All rights reserved.
+//
+
+#import "HPStudent.h"
+
+@interface HPStudent (Study)
+@property(nonatomic,copy)NSString* core;
+@end
---
+++ HPClassTool/HPStudent+Study.m
@@ -0,0 +1,13 @@
+//
+//  HPStudent+Study.m
+//  HPClassTool
+//
+//  Created by 王辉平 on 2018/8/30.
+//  Copyright © 2018年 王辉平. All rights reserved.
+//
+
+#import "HPStudent+Study.h"
+
+@implementation HPStudent (Study)
+
+@end
